3142ccd4deb1ee07936775f4b45b21e8258a5b95
Roman Molotov <r.molotov@yahoo.com>
2022-11-02 21:43:29 +0400

    layout based on screen size

--- Assets/CodeBase/UI/MenuController.cs
+++ Assets/CodeBase/UI/MenuController.cs
@@ -1,17 +1,54 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using CodeBase.Infrastructure;
 
 namespace CodeBase.UI
 {
     public class MenuController : MonoBehaviour
     {
+        private const float ImageSize = 0.5f;
+        private const float ContainerSpacing = 0.1f;
+        private const float ContainerPaddingHorizontal = 0.1f;
+        private const float ContainerPaddingVertical = 0.1f;
+        private const float ButtonPadding = 0.05f;
+
         [SerializeField] private MenuItemDetailsPopup menuItemDetailsPopup;
-        [SerializeField] private RectTransform itemsContainer;
+        [SerializeField] private GridLayoutGroup itemsContainer;
+        [SerializeField] private VerticalLayoutGroup buttonsContainer;
 
         private void Awake()
         {
+            SetLayout();
+            
             //for debug layout and interaction
             foreach (var menuItemView in itemsContainer.GetComponentsInChildren<MenuItemView>())
                 menuItemView.OnClick += arg => menuItemDetailsPopup.InitAndShow(arg, arg);
+        }
+        
+        private void SetLayout()
+        {
+            var screenHeight = DeviceInfoService.Instance.GetScreenHeight();
+
+            SetGridLayout(screenHeight);
+            SetButtonLayout(screenHeight);
+        }
+
+        private void SetGridLayout(float screenHeight)
+        {
+            itemsContainer.cellSize = screenHeight * ImageSize * Vector2.one;
+            itemsContainer.spacing = screenHeight * ContainerSpacing * Vector2.one;
+            itemsContainer.padding = new RectOffset(
+                (int) (screenHeight * ContainerPaddingVertical),
+                (int) (screenHeight * ContainerPaddingVertical),
+                (int) (screenHeight * ContainerPaddingHorizontal),
+                (int) (screenHeight * ContainerPaddingHorizontal)
+            );
+        }
+
+        private void SetButtonLayout(float screenHeight)
+        {
+            ((RectTransform) buttonsContainer.transform).anchoredPosition = ButtonPadding * screenHeight * new Vector2(-1, 1);
+            buttonsContainer.spacing = ButtonPadding * screenHeight;
         }
     }
 }
--- Assets/CodeBase/UI/MenuItemDetailsPopup.cs
+++ Assets/CodeBase/UI/MenuItemDetailsPopup.cs
@@ -1,17 +1,59 @@
-﻿using UnityEngine;
+﻿using System.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.UI;
+using CodeBase.Infrastructure;
 
 namespace CodeBase.UI
 {
     public class MenuItemDetailsPopup : PopupBase
     {
+        private const float ImageSize = 0.8f;
+        private const float TextSize = 0.08f;
+        private const float ButtonPadding = 0.05f;
+        
+        [SerializeField] private CanvasScaler canvasScaler;
         [SerializeField] private Image itemImage;
-        public override void InitAndShow<T>(T data, string text = "")
+        [SerializeField] private RectTransform textContainer;
+
+        public override async void InitAndShow<T>(T data, string text = "")
         {
             var spriteName = data as string;
-            itemImage.sprite = Resources.Load<Sprite>(spriteName);
+            await LoadSprite(spriteName);
             
+            SetLayout();
+
             base.InitAndShow(data, text);
+        }
+
+        private async Task LoadSprite(string spriteName)
+        {
+            var sprite = await AssetsService.Instance.GetSpriteById(spriteName);
+            itemImage.sprite = sprite;
+        }
+
+        private void SetLayout()
+        {
+            var screenHeight = DeviceInfoService.Instance.GetScreenHeight();
+
+            SetImageSize(screenHeight);
+            SetTextSize(screenHeight);
+            SetButtonLayout(screenHeight);
+        }
+
+        private void SetButtonLayout(float screenHeight)
+        {
+            ((RectTransform) okButton.transform).anchoredPosition = ButtonPadding * screenHeight * new Vector2(1, -1);
+        }
+
+        private void SetTextSize(float screenHeight)
+        {
+            popupText.rectTransform.sizeDelta = TextSize * screenHeight * Vector2.one;
+            textContainer.sizeDelta = (.5f * (1 - ImageSize)) * screenHeight * Vector2.one;
+        }
+
+        private void SetImageSize(float screenHeight)
+        {
+            itemImage.rectTransform.sizeDelta = ImageSize * screenHeight * Vector2.one;
         }
     }
 }
