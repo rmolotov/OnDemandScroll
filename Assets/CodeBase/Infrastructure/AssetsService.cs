f4cf144cbfc7b7162395e2a8c9777a67b21e4d81
Roman Molotov <r.molotov@yahoo.com>
2022-11-02 21:42:56 +0400

    asset managment and device info

---
+++ Assets/CodeBase/Infrastructure/AssetsService.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace CodeBase.Infrastructure
+{
+    public class AssetsService : MonoBehaviour
+    {
+        #region Singleton
+
+        private void Awake() => Instance ??= this;
+        public static AssetsService Instance { get; private set; }
+
+        #endregion
+
+        [SerializeField] private Sprite spritePlaceholder;
+        
+        private Dictionary<string, AsyncOperationHandle<Sprite>> _spawnedSprites;
+        
+        [ItemCanBeNull]
+        public async Task<Sprite> GetSpriteById(string id)
+        {
+            var operation = Addressables.LoadAssetAsync<Sprite>($"{id}");
+
+            if (operation.OperationException != null)
+                operation = Addressables.LoadAssetAsync<Sprite>(spritePlaceholder.name);
+
+            _spawnedSprites ??= new Dictionary<string, AsyncOperationHandle<Sprite>>();
+
+            if (!_spawnedSprites.ContainsKey(id)) 
+                _spawnedSprites.Add(id, operation);
+            else 
+                return await _spawnedSprites[id].Task;
+
+            return await operation.Task;
+        }
+        
+        public void ReleaseSprite(string id)
+        {
+            if (!_spawnedSprites.ContainsKey(id)) return;
+            
+            Addressables.Release(_spawnedSprites[id]);
+            _spawnedSprites.Remove(id);
+        }
+    }
+}
---
+++ Assets/CodeBase/Infrastructure/DeviceInfoService.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CodeBase.Infrastructure
+{
+    public class DeviceInfoService : MonoBehaviour
+    {
+        #region Singleton
+
+        private void Awake() => Instance ??= this;
+        public static DeviceInfoService Instance { get; private set; }
+
+        #endregion
+        
+        [SerializeField] private CanvasScaler canvasScaler;
+
+        public float GetScreenHeight() =>
+            canvasScaler
+                ? canvasScaler.referenceResolution.y
+                : Screen.height;
+    }
+}
